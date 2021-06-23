b7613c0a4d435348d247431ad019de0404597b22
Mehdi Echchelh <mehdi.echel@gmail.com>
2021-06-23 23:51:44 +0200

    [ADD] Data as internal data to the package

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' AIS Dataset
+#'
+#' A dataset containing marine data transmitted by the AIS
+#' (Automatic identification system) standard.
+#'
+#' @format A data frame with 3102887 rows and 20 variables:
+#' \describe{
+#'   \item{LAT}{ship’s latitude}
+#'   \item{LON}{ship’s longitude}
+#'   ...
+#' }
+"ships"
--- data-raw/ships.R
+++ data-raw/ships.R
@@ -1,3 +1,12 @@
 ## code to prepare `ships` dataset goes here
+ships <- read.csv("inst/extdata/ships.csv")
+
+# Data preprocessing
+ships <- ships %>% mutate_each(list(factor),
+                               c(DESTINATION, FLAG,
+                                 SHIPNAME, PORT,
+                                 ship_type, port))
+
+# ships <- ships[1:2000, ]
 
 usethis::use_data(ships, overwrite = TRUE)
