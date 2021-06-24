8d50ed8b16ea0fd1bb1d2f4c005e9a980f72b3f8
Mehdi Echchelh <mehdi.echel@gmail.com>
2021-06-24 07:53:20 +0200

    [UPDATED][app_ui] Updated grid structure

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,29 +10,43 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-    # Your application UI logic 
     semanticPage(
-      # --- Header
-      h1(class = "ui header", icon("ship"),
-         div(class = "content", "Ships dataset (AIS data)",
-             div(class = "sub header", "Appsilon Homework Assignement"))),
-      
-      # --- Menu
-      horizontal_menu(list(
-        list(name = "Home", link = "#", icon = "home"),
-        list(name = "Ships statistics", link = "#", icon = "ship")
-      )),
-      
-      # --- Plot
-      leafletOutput("ship-map"),
-      # --- Vessel selection (dropdown_input)
-      mod_select_vessel_ui("vessel_selection"),
-      # --- Message
-      div(
-        class = "ui raised segment",
-        div(
-          tags$span(class="ui blue ribbon label", "Vessel longest distance:"),
-          textOutput("vessel_distance")
+      tags$div(
+        class="ui container",
+        tags$div(
+          class="ui grid row",
+          # --- Header
+          h1(class = "ui header row", icon("ship"),
+             div(class = "content", "Ships dataset (AIS data)",
+                 div(class = "sub header", "Appsilon Homework Assignement"))),
+          
+          # --- Menu
+          horizontal_menu(list(
+            list(name = "Home", link = "#", icon = "home"),
+            list(name = "Ships statistics", icon = "ship", class = "disabled")
+          )),
+          
+          tags$div(
+            class="ui stackable column row card",
+            # --- Plot
+            tags$div(
+              class="ten wide column",
+              leafletOutput("ship-map")
+            ),
+            tags$div(
+              class="six wide column",
+              # --- Vessel selection (dropdown_input)
+              mod_select_vessel_ui("vessel_selection"),
+              # --- Message
+              div(
+                class = "ui raised segment",
+                div(
+                  tags$span(class="ui blue ribbon label", "Vessel longest distance:"),
+                  textOutput("vessel_distance")
+                )
+              )
+            )
+          )
         )
       )
     )
